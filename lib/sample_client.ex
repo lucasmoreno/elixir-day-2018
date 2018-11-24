9f3bc23a56fd7d8f92b9ba9c7b44ff4994b7026d
otto <otto@MacBook-Pro-de-quero.local>
2018-11-24 12:32:18 -0200

    simple client with doc

--- lib/sample_client.ex
+++ lib/sample_client.ex
@@ -1,52 +1,37 @@
 defmodule SampleClient do
   @host "http://canvas.elixirday.com"
-  @token "marcelo.koga"
+  @token "COLOQUE_SEU_TOKEN"
 
-  ### API ###
-  def move(direction) do
-    post("/api/move", direction: direction)
-  end
+  # POST
+  # /api/move
+  # Move o bot no canvas
+  # Parametros
+  # direction 
+  #        N
+  #    NW    NE
+  #  W           E
+  #    SW    SE
+  #       S
 
-  def paint, do: post("/api/paint")
+  # /api/change_color
+  # Muda a cor do bot
+  # Parametros
+  # color - #ff0000
+  
+  # /api/paint
+  # Pinta a posição atual do bot
 
-  def change_color(color), do: post("/api/change_color", %{ color: color })
+  # /api/locate
+  # Retorna todos os pixels em volta da posição do bot
 
-  def locate() do
-    post("/api/locate")
-  end
-
-  def desenha_quadrado(tamanho) do
-    ["E", "S", "W", "N"]
-    |> Enum.each(fn(d) -> desenha_linha(d, tamanho) end)
-  end
-
-  def zigue_zague(altura) do
-    1..altura
-    |> Enum.each(fn(_) ->
-      change_color("#babaca")
-      desenha_linha("NE", 15)
-      change_color("#dedada")
-      desenha_linha("NW", 15)
-    end)
-  end
-
-  def desenha_linha(d, tamanho) do
-    (1..tamanho)
-    |> Enum.each(fn(_) -> move_and_paint(d) end)
-  end
-
-  def move_and_paint(d) do
-    move(d)
-    paint()
-  end
-
-  defp post(url, params \\ %{}) do
+  # Funções aulixiares
+  def post(url, params \\ %{}) do
     HTTPoison.post!(@host <> url,
       params_and_token(params),
       [{"Content-type", "application/json"}])
   end
 
-  defp params_and_token(params) do
+  def params_and_token(params) do
     params
     |> Map.merge(%{token: @token})
     |> Poison.encode!()
